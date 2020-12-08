ae7f7560f283a3b936a129cb3255353e85e934ec
sowiecky <michalsochowski@gmail.com>
2020-12-08 16:00:28 +0100

    Dziala dodawanie zarowek do grup

--- src/main/java/inz/restapiproject/controller/MqttController.java
+++ src/main/java/inz/restapiproject/controller/MqttController.java
@@ -1,8 +1,5 @@
 package inz.restapiproject.controller;
 
-
-import com.google.gson.Gson;
-import inz.restapiproject.model.Groups;
 import inz.restapiproject.model.Lights;
 import inz.restapiproject.service.GroupsService;
 import inz.restapiproject.service.MqttService;
@@ -10,8 +7,6 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -23,44 +18,19 @@ public class MqttController {
     @Autowired
     GroupsService groupsService;
 
-//    @GetMapping("/sendInfo")
-//    public @ResponseBody String sendInfo(@RequestParam String color , @RequestParam String str) {
-//
-//        String msg_color = color;
-//
-//        String[] topic = str.split("\\s+");
-//
-//        for(int i =0 ; i<topic.length; i++){
-//            System.out.println(topic[i]);
-//        }
-//        for (int i=0; i<topic.length; i++) {
-//            try {
-//                System.out.println(topic);
-//                mqttService.publish(topic[i], msg_color, 0, false);
-//
-//            } catch (MqttException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//        return "Koniec";
-//    }
-
     @GetMapping("/sendInfo")
     public @ResponseBody String sendInfo(@RequestParam String message , @RequestParam String groupId) {
 
-        String messageToMqtt = message.replace("_", " ");
-        Groups group = groupsService.getGroupById(Long.parseLong(groupId));
-        Set<Lights> lights = group.getLights();
+        String messageToMqtt = message.replace("_", " ");                       //dostosowywanie wiadomości
+        Set<Lights> lights = groupsService.getGroupById(Long.parseLong(groupId)).getLights();   //pobieranie zarowek z wybranej grupy
 
         for (Lights light: lights) {
             try {
-                mqttService.publish(light.getSerial(),messageToMqtt, 0, false);
+                mqttService.publish(light.getSerial(),messageToMqtt, 0, false);     //dla kazdej zarowki wysyłanie tej samej wiadomosci
             } catch (MqttException e) {
                 e.printStackTrace();
             }
         }
-
         return "Wyslane";
     }
 }
